7ae0008da6aa69533c2fb607cfc58356624a620b
Jesimiel Almedejar <jesimielalmedejar1018@gmail.com>
2022-06-05 21:07:55 +0800

    added some basic js event

---
+++ script.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector("[data-type-button]");
+
+// create a new instance of the SpeechRecognition object
+btn.addEventListener("click", () => {
+   alert("Hello World");
+});
